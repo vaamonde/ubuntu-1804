3f5dcfb3df3cd0e022cc75c5d694655cd12f162e
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-12-20 19:38:54 -0200

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo -en "Verificando conflito de Software, aguarde...\n"
+echo -n "Verificando conflito de Software, aguarde... "
 	for name in mysql-server mysql-common apache2
 	do
-  		[[ $(dpkg -s $name 2> /dev/null) ]] || { echo -en "Conflito de Software do: $name .: OK\n";deps=1; }
+  		[[ $(dpkg -s $name 2> /dev/null) ]] || { echo -en "\n\nO software: $name precisa ser instalado. \nUse o comando 'apt install $name'\n";deps=1; }
 	done
-		[[ $deps -ne 1 ]] && { echo -en "\n\nO software: $name precisa ser removido. \nUse o comando 'apt purge $name'\n";exit 1; } || echo "Conflito de Software.: OK"
+		[[ $deps -ne 1 ]] && echo "Dependências.: OK" || { echo -en "\nInstale as dependências acima e execute novamente este script\n";exit 1; }
 		sleep 5
