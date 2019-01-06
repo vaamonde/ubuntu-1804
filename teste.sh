fd581d5cccf7900ff921e72764ec175d109eaa12
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-01-06 20:43:40 -0200

    Delete teste.sh

--- teste.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "Verificando conflito de Software, aguarde... "
-	for name in mysql-server mysql-common apache2
-	do
-  		[[ $(dpkg -s $name 2> /dev/null) ]] || { echo -en "\n\nO software: $name precisa ser instalado. \nUse o comando 'apt install $name'\n";deps=1; }
-	done
-		[[ $deps -ne 1 ]] && echo "Dependências.: OK" || { echo -en "\nInstale as dependências acima e execute novamente este script\n";exit 1; }
-		sleep 5
