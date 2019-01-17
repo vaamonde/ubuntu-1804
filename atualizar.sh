7c8d72e9db6e1dfe9d6355c8e270636bccf9f6d2
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-01-17 16:33:46 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update
 echo
 echo Listas atualizadas
+sleep 5
+clear
 
 echo Atualizando os Software
 echo
 	sudo apt -y upgrade
 echo
 echo Software atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo
 	sudo apt -y dist-upgrade
 echo
 echo Kernel atualizados
+sleep 5
+clear
 
 echo Removendo Software
 echo
 	sudo apt -y autoremove
 echo
 echo Software removidos
+sleep 5
+clear
 
 echo Limpando o Cache
 echo
 	sudo apt -y autoclean
 echo
 echo Cache Limpo
+sleep 5
+clear
