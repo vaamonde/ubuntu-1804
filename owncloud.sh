d5c427bcd264829122e31f00a2a0009c31a6a48a
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-02-10 15:56:18 -0200

    Update owncloud.sh

--- owncloud.sh
+++ owncloud.sh
@@ -5,8 +5,8 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 08/02/2019
-# Data de atualização: 08/02/2019
-# Versão: 0.01
+# Data de atualização: 10/02/2019
+# Versão: 0.02
 # Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
 # Kernel >= 4.15.x
 # Testado e homologado para a versão do ownCLOUD-10.1.x
@@ -20,6 +20,7 @@
 # Vídeo de atualização do Sistema: https://www.youtube.com/watch?v=esnu8TAepHU
 # Vídeo de configuração da Placa de Rede: https://www.youtube.com/watch?v=zSUd4k108Zk
 # Vídeo de configuração do Hostname e Hosts: https://www.youtube.com/watch?v=J7eyb5ynjZA
+# Vídeo de instalação do LAMP Server no Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=6EFUu-I3
 #
 # Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
 # opção do comando date: +%T (Time)
@@ -43,8 +44,22 @@ KERNEL=`uname -r | cut -d'.' -f1,2`
 # $0 (variável de ambiente do nome do comando)
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 #
-# Variáveis de instalação do ownCLOUD
-RELEASE="https://download.owncloud.org/download/repositories/production/Ubuntu_18.04/Release.key"
+# opção do comando create: create (criação), database (base de dados), base (banco de dados)
+# opção do comando create: create (criação), user (usuário), identified by (indentificado por - senha do usuário), password (senha)
+# opção do comando grant: grant (permissão), usage (uso em | uso na), *.* (todos os bancos/tabelas), to (para), user (usário)
+# identified by (indentificado por - senha do usuário), password (senha)
+# opões do comando GRANT: grant (permissão), all (todos privilegios), on (em ou na | banco ou tabela), *.* (todos os bancos/tabelas)
+# to (para), user@'%' (usuário @ localhost), identified by (indentificado por - senha do usuário), password (senha)
+# opção do comando FLUSH: flush (atualizar), privileges (recarregar as permissões)
+USER="root"
+PASSWORD="pti@2018"
+DATABASE="CREATE DATABASE owncloud;"
+USERDATABASE="CREATE USER 'owncloud'@'localhost' IDENTIFIED BY 'owncloud';"
+GRANTALL="GRANT ALL ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'ownlocud' WITH GRANT OPTION;"
+FLUSH="FLUSH PRIVILEGES;"
+#
+# Variáveis de instalação do ownCloud
+RELEASE="https://download.owncloud.org/community/owncloud-10.1.0.tar.bz2"
 #
 # Verificando se o usuário e Root, Distribuição e >=18.04 e o Kernel >=4.15 <IF MELHORADO)
 # [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
@@ -61,14 +76,27 @@ if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
 		echo -e "Execute novamente o script para verificar o ambiente."
 		exit 1
 fi
-#		
-# Script de instalação do ownCLOUD no GNU/Linux Ubuntu Server 18.04.x
+#
+#
+# Verificando se as dependêncais do ownCloud estão instaladas
+# opção do dpkg: -s (status), opção do echo: -e (intepretador de escapes de barra invertida), -n (permite nova linha)
+# || (operador lógico OU), 2> (redirecionar de saída de erro STDERR), && = operador lógico AND, { } = agrupa comandos em blocos
+# [ ] = testa uma expressão, retornando 0 ou 1, -ne = é diferente (NotEqual)
+echo -n "Verificando as dependências, aguarde... "
+	for name in mysql-server mysql-common apache2
+	do
+  		[[ $(dpkg -s $name 2> /dev/null) ]] || { echo -en "\n\nO software: $name precisa ser instalado. \nUse o comando 'apt install $name'\n";deps=1; }
+	done
+		[[ $deps -ne 1 ]] && echo "Dependências.: OK" || { echo -en "\nInstale as dependências acima e execute novamente este script\n";exit 1; }
+		sleep 5
+#
+# Script de instalação do ownCloud no GNU/Linux Ubuntu Server 18.04.x
 # opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
 # opção do comando hostname: -I (all IP address)
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
 echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
 #
-echo -e "Instalação do ownCLOUD no GNU/Linux Ubuntu Server 18.04.x\n"
+echo -e "Instalação do ownCloud no GNU/Linux Ubuntu Server 18.04.x\n"
 echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet...\n"
 sleep 5
 #
@@ -109,38 +137,41 @@ echo -e "Software removidos com sucesso!!!, continuando com o script..."
 sleep 5
 clear
 #
-echo -e "Instalando o ownCLOUD, aguarde..."
-echo
+echo -e "Instalando o ownCloud, aguarde...\n"
 #
-echo -e "Adicionando o repositório de versões do ownCLOUD, aguarde..."
+echo -e "Habilitando os recursos do Apache2, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando: < (redirecionar a entrada padrão)
-	# opção do comando wget: -n (), -v (), -O ()
-	# opção do comando apt-key: add (), - ()
-	wget -nv $RELEASE -O Release.key &>> $LOG
-	apt-key add - < Release.key &>> $LOG
-echo -e "Repositório instaladas com sucesso!!!, continuando com o script..."
+	a2dismod autoindex &>> $LOG
+	a2enmod rewrite &>> $LOG
+	a2enmod headers &>> $LOG
+	a2enmod env &>> $LOG
+	a2enmod dir &>> $LOG
+	a2enmod mime &>> $LOG
+echo -e "Recursos habilitados com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
-echo -e "Criando o arquivo de Source List do ownCLOUD, aguarde..."
+echo -e "Instalando as dependências do ownCloud, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando cp: -v (verbose)
-	cp -v conf/owncloud.list /etc/apt/sources.list.d/owncloud.list &>> $LOG
-	apt update &>> $LOG
+	apt -y install software-properties-common &>> $LOG
+	apt -y install php-cli php-common php-mbstring php-gd php-intl php-xml php-mysql php-zip php-curl php-xmlrpc &>> $LOG
+	systemctl restart apache2.service &>> $LOG
 echo -e "Source List criado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
-echo -e "Instalando o ownCLOUD, aguarde..."
+echo -e "Instalando o ownCloud, aguarde..."
 	# opção do comando: &>> (redirecionar a saida padrão)
-	# opção do comando apt: -y (yes)
-	apt -y install owncloud-files &>> $LOG
+	wget $RELEASE &>> $LOG
+	tar -zxvf ownlocud-* &>> $LOG
+	mv -v owncloud*/ /var/www/html/own/ &>> $LOG
+	chown -R www-data:www-data /var/www/html/own/ &>> $LOG
+	chmod -R 755 /var/www/html/own/ &>> $LOG
 echo -e "ownCLOUD instalado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
-echo -e "Instalação do ownCLOUD feita com Sucesso!!!."
+echo -e "Instalação do ownCloud feita com Sucesso!!!."
 	# script para calcular o tempo gasto (SCRIPT MELHORADO, CORRIGIDO FALHA DE HORA:MINUTO:SEGUNDOS)
 	# opção do comando date: +%T (Time)
 	HORAFINAL=`date +%T`
