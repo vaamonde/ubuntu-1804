b19c5a8568da2db1aab3760dbdb75ae60897a39c
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-02-10 14:49:39 -0200

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -5,14 +5,10 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 09/11/2018
-# Data de atualização: 29/01/2019
-# Versão: 0.04
+# Data de atualização: 10/02/2019
+# Versão: 0.05
 # Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
 # Kernel >= 4.15.x
-#
-# Vídeo de instalação do GNU/Linux Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=zDdCrqNhIXI
-#
-# Vídeo de instalação do LAMP Server no Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=6EFUu-I3u4s
 #
 # Wordpress: é um sistema livre e aberto de gestão de conteúdo para internet (do inglês: Content Management System - CMS),
 # baseado em PHP com banco de dados MySQL, executado em um servidor interpretador, voltado principalmente para a criação de
@@ -20,6 +16,9 @@
 # distribuído gratuitamente sob a GNU General Public License.
 #
 # Site oficial: https://wordpress.org/
+#
+# Vídeo de instalação do GNU/Linux Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=zDdCrqNhIXI
+# Vídeo de instalação do LAMP Server no Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=6EFUu-I3u4s
 #
 # Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
 # opção do comando date: +%T (Time)
@@ -36,19 +35,10 @@ USUARIO=`id -u`
 UBUNTU=`lsb_release -rs`
 KERNEL=`uname -r | cut -d'.' -f1,2`
 #
-# Variável do caminho do Log dos Script utilizado nesse curso
-VARLOGPATH="/var/log/"
-#
-# Variável para criação do arquivo de Log dos Script
-# $0 (variável de ambiente do nome do comando)
-# opção do caracter: | (piper) Conecta a saída padrão com a entrada padrão de outro comando
-# opção do shell script: acento crase ` ` = Executa comandos numa subshell, retornando o resultado
-# opção do shell script: aspas simples ' ' = Protege uma string completamente (nenhum caractere é especial)
+# Variável do caminho do Log dos Script utilizado nesse curso (VARIÁVEL MELHORADA)
 # opções do comando cut: -d (delimiter), -f (fields)
-LOGSCRIPT=`echo $0 | cut -d'/' -f2`
-#
-# Variável do caminho para armazenar os Log's de instalação
-LOG=$VARLOGPATH/$LOGSCRIPT
+# $0 (variável de ambiente do nome do comando)
+LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 #
 # Declarando as variaveis para o download do Wordpress
 WORDPRESS="https://wordpress.org/latest.zip"
@@ -57,16 +47,16 @@ WORDPRESS="https://wordpress.org/latest.zip"
 USER="root"
 PASSWORD="pti@2018"
 # opção do comando create: create (criação), database (base de dados), base (banco de dados)
-DATABASE="CREATE DATABASE wordpress;"
 # opção do comando create: create (criação), user (usuário), identified by (indentificado por - senha do usuário), password (senha)
-USERDATABASE="CREATE USER 'wordpress' IDENTIFIED BY 'wordpress';"
 # opção do comando grant: grant (permissão), usage (uso em | uso na), *.* (todos os bancos/tabelas), to (para), user (usário)
 # identified by (indentificado por - senha do usuário), password (senha)
-GRANTDATABASE="GRANT USAGE ON *.* TO 'wordpress' IDENTIFIED BY 'wordpress';"
 # opões do comando GRANT: grant (permissão), all (todos privilegios), on (em ou na | banco ou tabela), *.* (todos os bancos/tabelas)
 # to (para), user@'%' (usuário @ localhost), identified by (indentificado por - senha do usuário), password (senha)
-GRANTALL="GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress';"
 # opção do comando FLUSH: flush (atualizar), privileges (recarregar as permissões)
+DATABASE="CREATE DATABASE wordpress;"
+USERDATABASE="CREATE USER 'wordpress' IDENTIFIED BY 'wordpress';"
+GRANTDATABASE="GRANT USAGE ON *.* TO 'wordpress' IDENTIFIED BY 'wordpress';"
+GRANTALL="GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress';"
 FLUSH="FLUSH PRIVILEGES;"
 #
 # Verificando se o usuário e Root, Distribuição e >=18.04 e o Kernel >=4.15 <IF MELHORADO)
@@ -100,14 +90,14 @@ sleep 5
 echo
 #
 echo -e "Atualizando as listas do Apt, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	apt update &>> $LOG
 echo -e "Listas atualizadas com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
 echo -e "Atualizando o sistema, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes)
 	apt -y upgrade &>> $LOG
 echo -e "Sistema atualizado com sucesso!!!, continuando com o script..."
@@ -115,7 +105,7 @@ sleep 5
 echo
 #
 echo -e "Instalando as dependências do Wordpress, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes)
 	apt -y install unzip &>> $LOG
 echo -e "Dependências instaladas com sucesso!!!, continuando com o script"
@@ -123,7 +113,7 @@ sleep 5
 echo
 #
 echo -e "Removendo os software desnecessários, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes)
 	apt -y autoremove &>> $LOG
 echo -e "Software removidos com Sucesso!!!, continuando com o script..."
@@ -134,21 +124,21 @@ echo -e "Instalando o Wordpress, aguarde..."
 echo
 #
 echo -e "Baixando o Wordpress do site oficial, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	wget $WORDPRESS &>> $LOG
 echo -e "Wordpress baixado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
 echo -e "Descompactando o Wordpress, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	unzip latest.zip &>> $LOG
 echo -e "Descompactação feita com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
 echo -e "Copiando os arquivos de configuração do Wordpress, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando mv: -v (verbose)
 	# opção do comando cp: -v (verbose)
 	mv -v wordpress/ /var/www/html/wp &>> $LOG
@@ -159,7 +149,7 @@ sleep 5
 echo
 #				 
 echo -e "Alterando as permissões dos arquivos e diretórios do Wordpress, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando chmod: -R (recursive), -f (silent), -v (verbose), 755 (Dono=RWX,Grupo=R-X,Outros=R-X)
 	# opção do comando chown: -R (recursive), -f (silent), -v (verbose), dono.grupo (alteraçaõ do dono e grupo)
 	chmod -Rfv 755 /var/www/html/wp/ &>> $LOG
@@ -169,7 +159,7 @@ sleep 5
 echo
 #
 echo -e "Criando a Base de Dados do Wordpress, aguarde..."
-	# opção do comando: &>> (redirecionar a entrada padrão)
+	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando mysql: -u (user), -p (password) -e (execute)
 	mysql -u $USER -p$PASSWORD -e "$DATABASE" mysql &>> $LOG
 	mysql -u $USER -p$PASSWORD -e "$USERDATABASE" mysql &>> $LOG
