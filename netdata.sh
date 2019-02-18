0822c5c0d09b0c3196116d6b0074369c50342168
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-02-18 10:33:58 -0300

    Update netdata.sh

--- netdata.sh
+++ netdata.sh
@@ -124,7 +124,7 @@ echo
 echo -e "Instalando as dependências do Netdata, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes), \ (bar left) quedra de linha na opção do apt
-	apt -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config uuid-dev python python-mysqldb python-pip \ 
+	apt -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config uuid-dev python python-mysqldb python-pip \
 	python-dev python3-dev libmysqlclient-dev python-ipaddress &>> $LOG
 echo -e "Instalação das dependências feita com sucesso!!!, continuando com o script..."
 sleep 5
