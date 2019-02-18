7c902161cc18d0d98696d7371c323d5a6c13a9e2
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-02-18 11:01:26 -0300

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -143,7 +143,7 @@ echo -e "Instalando as dependências do Asterisk, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes) | $(uname -r) = kernel-release, \ (bar left) quedra de linha na opção do apt
 	apt install -y build-essential libssl-dev libelf-dev libncurses5-dev libnewt-dev libxml2-dev linux-headers-$(uname -r) \
-	libsqlite3-dev uuid-dev subversion libjansson-dev sqlite3 autoconf automake libtool libedit-dev flex bison libtool \ 
+	libsqlite3-dev uuid-dev subversion libjansson-dev sqlite3 autoconf automake libtool libedit-dev flex bison libtool \
 	libtool-bin unzip sox openssl zlib1g-dev unixodbc unixodbc-dev &>> $LOG
 echo -e "Dependências instaladas com sucesso!!!, continuando com o script..."
 sleep 5
