896a595205e30c4225a1b950b90b652462b26898
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-02-28 20:08:12 -0300

    Update wp-config.php

--- conf/wp-config.php
+++ conf/wp-config.php
@@ -6,8 +6,8 @@
  * Facebook: facebook.com/BoraParaPratica
  * YouTube: youtube.com/BoraParaPratica
  * Data de criação: 04/11/2018
- * Data de atualização: 04/11/2018
- * Versão: 0.01
+ * Data de atualização: 28/02/2019
+ * Versão: 0.02
  * Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
  * Kernel >= 4.15.x
 */
