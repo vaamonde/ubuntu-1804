4fefd2cb464fc288eb0d70692fed5cd25e0864a1
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-10-22 16:17:05 -0300

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -108,7 +108,7 @@ echo -e "Instalando as dependências do Webmin, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes), \ (Bar, opção de quebra de linha no apt)
 	apt -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions \
-	python &>> $LOG
+	python unzip &>> $LOG
 echo -e "Instalação das dependências feita com sucesso!!!, continuando com o script..."
 sleep 5
 echo
