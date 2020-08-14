c7973e7c45eb2c72d7d919d33a75d7b515780a9e
Robson Vaamonde <robsonvaamonde@gmail.com>
2020-08-14 17:22:02 -0300

    Atualizacao

--- fusion.sh
+++ fusion.sh
@@ -5,11 +5,11 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 06/08/2020
-# Data de atualização: 06/08/2020
-# Versão: 0.01
+# Data de atualização: 14/08/2020
+# Versão: 0.02
 # Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
 # Kernel >= 4.15.x
-# Testado e homologado para a versão do FusionInventory Server 9.5.x, Agent 2.5.x
+# Testado e homologado para a versão do FusionInventory Server 9.5.x, Agent 2.5.x, GLPI 9.4.x
 #
 # O FusionInventory Agent é um agente multiplataforma genérico. Ele pode executar uma grande variedade de 
 # tarefas de gerenciamento, como inventário local, implantação de software ou descoberta de rede. Ele pode 
@@ -22,7 +22,7 @@
 #
 # Vídeo de instalação do GNU/Linux Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=zDdCrqNhIXI
 # Vídeo de instalação do LAMP Server: https://www.youtube.com/watch?v=6EFUu-I3u4s&t
-# Vídeo de instalação do GLPI Help Desk: 
+# Vídeo de instalação do GLPI Help Desk: https://www.youtube.com/watch?v=6T9dMwJMeDw&t
 #
 # Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
 # opção do comando date: +%T (Time)
@@ -46,13 +46,19 @@ KERNEL=$(uname -r | cut -d'.' -f1,2)
 # $0 (variável de ambiente do nome do comando)
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 #
-# Declarando as variáveis de download do FusionInventory (Links atualizados no dia 06/08/2020)
+# Declarando as variáveis de download do FusionInventory (Links atualizados no dia 14/08/2020)
 FUSIONSERVER="https://github.com/fusioninventory/fusioninventory-for-glpi/releases/download/glpi9.5.0%2B1.0/fusioninventory-9.5.0+1.0.tar.bz2"
 FUSIONAGENT="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent_2.5.2-1_all.deb"
-FUSIONTASK="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent-task-collect_2.5.2-1_all.deb"
+FUSIONCOLLECT="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent-task-collect_2.5.2-1_all.deb"
 FUSIONNETWORK="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent-task-network_2.5.2-1_all.deb"
 FUSIONDEPLOY="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent-task-deploy_2.5.2-1_all.deb"
+AGENTWINDOWS32="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent_windows-x86_2.5.2.exe"
+AGENTWINDOWS64="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent_windows-x64_2.5.2.exe"
+AGENTMACOS="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/FusionInventory-Agent-2.5.2-1.dmg"
 GLPI="/var/www/html/glpi"
+#
+# Exportando o recurso de Noninteractive do Debconf para não solicitar telas de configuração
+export DEBIAN_FRONTEND="noninteractive"
 #
 # Verificando se o usuário é Root, Distribuição é >=18.04 e o Kernel é >=4.15 <IF MELHORADO)
 # [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
@@ -60,7 +66,7 @@ clear
 if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
 	then
 		echo -e "O usuário é Root, continuando com o script..."
-		echo -e "Distribuição é >=18.04.x, continuando com o script..."
+		echo -e "Distribuição é >= 18.04.x, continuando com o script..."
 		echo -e "Kernel é >= 4.15, continuando com o script..."
 		sleep 5
 	else
@@ -84,11 +90,11 @@ echo -n "Verificando as dependências, aguarde... "
 #
 # Verificando se o GLPI Help Desk está instaladas
 # opção do comando: echo: -e (interpretador de escapes de barra invertida)
-# opção do comando if: [ ] = testa uma expressão, -d = testa se é diretório existe
+# opção do comando if: [ ] = testa uma expressão, -e = testa se é diretório existe
 echo -e "Verificando se o GLPI Help Desk está instalado, aguarde...\n"
 	if [ -e "$GLPI" ]
 		then
-    		echo "O GLPI Help Desk está instalado, continuando com o script..."
+    		echo -e "O GLPI Help Desk está instalado, continuando com o script...\n"
 			sleep 5
 	else
     		echo "O GLPI Help Desk não está instalado, instale o GLPI com o script: glpi.sh"
@@ -103,14 +109,13 @@ echo -e "Verificando se o GLPI Help Desk está instalado, aguarde...\n"
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
 # opção do comando cut: -d (delimiter), -f (fields)
 echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
-#
 clear
+#
 echo -e "Instalação do FusionInventory no GNU/Linux Ubuntu Server 18.04.x\n"
 echo -e "Após a instalação do FusionInventory acesse a URL: http://`hostname -I | cut -d' ' -f1`/glpi\n"
 echo -e "As configurações do FusionInventory e feita dentro do GLPI Help Desk\n"
-echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet..."
+echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet...\n"
 sleep 5
-#
 #
 echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
@@ -147,6 +152,7 @@ echo -e "Removendo software desnecessários, aguarde..."
 	apt -y autoremove &>> $LOG
 echo -e "Software removidos com sucesso!!!, continuando com o script..."
 sleep 5
+echo
 #
 echo -e "Instalando o FusionInventory Server e Agent, aguarde...\n"
 #
@@ -162,7 +168,7 @@ echo
 #
 echo -e "Descompactando o FusionInventory Server, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando tar: -j (), -x (), -v (), -f ()
+	# opção do comando tar: -j (bzip2), -x (extract), -v (verbose), -f (file)
 	tar -jxvf fusion.tar.bz2 &>> $LOG
 echo -e "FusionInventory Server descompactado com sucesso!!!, continuando com o script..."
 sleep 5
@@ -171,7 +177,7 @@ echo
 echo -e "Movendo o diretório do FusionInventory Server para o GLPI Help Desk, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando mv: -v (verbose)
-	mv -v  $GLPI/plugins/ &>> $LOG
+	mv -v fusioninventory/ $GLPI/plugins/ &>> $LOG
 echo -e "Diretório do FusionInventory Server movido com sucesso!!!, continuando com o script..."
 sleep 5
 echo
@@ -179,13 +185,11 @@ echo
 echo -e "Instalando as Dependências do FusionInventory Server e Agent, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes)
-	# FusionInventory Agent:
-	apt -y install dmidecode hwdata ucf hdparm &>> $LOG
-	apt -y install perl libuniversal-require-perl libwww-perl libparse-edid-perl &>> $LOG
-	apt -y install libproc-daemon-perl libfile-which-perl libhttp-daemon-perl &>> $LOG
-	apt -y install libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl &>> $LOG
-	apt -y install libdigest-sha-perl libsocket-getaddrinfo-perl libtext-template-perl &>> $LOG
-	apt -y install libxml-xpath-perl libyaml-tiny-perl &>> $LOG
+	# FusionInventory Agent
+	apt -y install dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl \
+	libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl \
+	libnet-ip-perl libdigest-sha-perl libsocket-getaddrinfo-perl libtext-template-perl libxml-xpath-perl \
+	libyaml-tiny-perl &>> $LOG
 	# FusionInventory Task Network
 	apt -y install libnet-snmp-perl libcrypt-des-perl libnet-nbname-perl &>> $LOG
 	# FusionInventory SNMPv3 
@@ -204,7 +208,7 @@ echo -e "Fazendo o download do FusionInventory Agent do site Oficial, aguarde...
 	# opção do comando wget: -O (output document file)
 	rm -v agent.deb task.deb deploy.deb snmp.deb &>> $LOG
 	wget $FUSIONAGENT -O agent.deb &>> $LOG
-	wget $FUSIONTASK -O task.deb &>> $LOG
+	wget $FUSIONCOLLECT -O task.deb &>> $LOG
 	wget $FUSIONDEPLOY -O deploy.deb &>> $LOG
 	wget $FUSIONNETWORK -O network.deb &>> $LOG
 echo -e "Download do FusionInventory Agent feito com sucesso!!!, continuando com o script..."
@@ -238,7 +242,6 @@ echo -e "Iniciando o serviço do FusionInventory Agent, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	systemctl enable fusioninventory-agent &>> $LOG
 	systemctl restart fusioninventory-agent &>> $LOG
-	systemctl reload fusioninventory-agent &>> $LOG
 echo -e "Serviço do FusionInventory Agent iniciado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
@@ -247,6 +250,24 @@ echo -e "Executando o Inventário do FusionInventory Agent, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	fusioninventory-agent --debug &>> $LOG
 echo -e "Inventário do FusionInventory Agent feito com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
+echo -e "Criando o repositório local e fazendo o download dos Agentes do FusionInventory, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	# opção do comando mkdir: -v (verbose)
+	# opção do comando chown: -v (verbose)
+	# opção do comando chmod: -v (verbose)
+	# opção do comando cp: -v (verbose)
+	# opção do comando wget: -O (output document file)
+	mkdir -v /var/www/html/agentes &>> $LOG
+	chown -v www-data.www-data /var/www/html/agentes &>> $LOG
+	chmod -v 755 /var/www/html/agentes &>> $LOG
+	cp -v conf/agent.cfg /var/www/html/agentes &>> $LOG
+	wget $AGENTWINDOWS32 -O /var/www/html/agentes/agent_windows32.exe &>> $LOG
+	wget $AGENTWINDOWS64 -O /var/www/html/agentes/agent_windows64.exe &>> $LOG
+	wget $AGENTMACOS -O /var/www/html/agentes/agent_macos.dmg &>> $LOG
+echo -e "Download dos FusionInventory Agent feito com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
