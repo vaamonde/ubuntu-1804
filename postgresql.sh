d4cc0b60a0609ead614703846aecb6dfd7f76859
Robson Vaamonde <robsonvaamonde@gmail.com>
2021-01-16 10:39:01 -0300

    Atualizacao

--- postgresql.sh
+++ postgresql.sh
@@ -127,6 +127,14 @@ echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
+#
+echo -e "Adicionando o Repositório Multiversão do Apt, aguarde..."
+	# opção do comando: &>> (redirecionar a saída padrão)
+	add-apt-repository multiverse &>> $LOG
+echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
+sleep 5
+echo
+#
 echo -e "Atualizando as listas do Apt, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	apt update &>> $LOG
