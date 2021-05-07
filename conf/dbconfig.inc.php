c661e47a7505a27757dfb6a37b24907cfd6d4875
root <root@ptispo01ws01.pti.intra>
2021-05-07 13:21:48 -0300

    Atualizacao

---
+++ conf/dbconfig.inc.php
@@ -0,0 +1,13 @@
+<?php
+define("DB_NAME", "ocsweb");
+define("SERVER_READ","localhost");
+define("SERVER_WRITE","localhost");
+define("SERVER_PORT","3306");
+define("COMPTE_BASE","ocs");
+define("PSWD_BASE","ocs");
+define("ENABLE_SSL","0");
+define("SSL_MODE","");
+define("SSL_KEY","");
+define("SSL_CERT","");
+define("CA_CERT","");
+?>
